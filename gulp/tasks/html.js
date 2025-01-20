399546d91ea5041282caa5f2a3b756e636840f35
ggzombi <attilahead@gmail.com>
2025-01-20 22:49:19 +0500

    gulp

---
+++ gulp/tasks/html.js
@@ -0,0 +1,17 @@
+import gulp from 'gulp'
+import fileinclude from 'gulp-file-include'
+import webpHtmlNoSvg from 'gulp-webp-html-nosvg'
+import gulpif from 'gulp-if'
+import plumber from 'gulp-plumber'
+import config from '../config'
+
+export const htmlBuild = () =>
+  gulp
+    .src(`${config.src.html}/*.html`)
+    .pipe(plumber())
+    .pipe(fileinclude())
+    .pipe(gulpif(config.isProd, webpHtmlNoSvg()))
+    .pipe(gulp.dest(`${config.dest.html}`))
+
+export const htmlWatch = () =>
+  gulp.watch(`${config.src.html}/**/*.html`, htmlBuild)
